b2bac5bac47b38e090dedfa5e5cf059472f8eaf1
Mahendra <mahendra.vyas@dianapps.com>
2020-10-21 23:48:46 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,103 @@
+export default [
+    {
+      icon: 'social-tumblr',
+      name: `4'7"`,
+    },
+    {
+      icon: 'social-twitter',
+      name: `4'8"`,
+    },
+    {
+      icon: 'social-facebook',
+      name: `4'9"`,
+    },
+    {
+      icon: 'social-instagram',
+      name: `4'10"`,
+    },
+    {
+      icon: 'social-linkedin',
+      name: `4'11"`,
+    },
+    {
+      icon: 'social-pinterest',
+      name: `5'0"`,
+    },
+    {
+      icon: 'social-github',
+      name: `5'1"`,
+    },
+    {
+      icon: 'social-google',
+      name: `5'2"`,
+    },
+    {
+      icon: 'social-reddit',
+      name: `5'3"`,
+    },
+    {
+      icon: 'social-skype',
+      name: `5'4"`,
+    },
+    {
+      icon: 'social-dribbble',
+      name: `5'5"`,
+    },
+    {
+      icon: 'social-behance',
+      name: `5'6"`,
+    },
+    {
+      icon: 'social-foursqare',
+      name: `5'7"`,
+    },
+    {
+      icon: 'social-soundcloud',
+      name: `5'8"`,
+    },
+    {
+      icon: 'social-spotify',
+      name: `5'9"`,
+    },
+    {
+      icon: 'social-stumbleupon',
+      name: `5'10"`,
+    },
+    {
+      icon: 'social-youtube',
+      name: `5'11"`,
+    },
+    {
+      icon: 'social-dropbox',
+      name: `6'0"`,
+    },
+    {
+      icon: 'social-vkontakte',
+      name: `6'1"`,
+    },
+    {
+      icon: 'social-steam',
+      name: `6'2"`,
+    },
+    {
+      icon: 'social-steam',
+      name: `6'3"`,
+    },
+    {
+      icon: 'social-steam',
+      name: `6'4"`,
+    },
+    {
+      icon: 'social-steam',
+      name: `6'5"`,
+    },
+    {
+      icon: 'social-steam',
+      name: `6'6"`,
+    },
+    {
+      icon: 'social-steam',
+      name: `6'7"`,
+    },
+  ];
+  
